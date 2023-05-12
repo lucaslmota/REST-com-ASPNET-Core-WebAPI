173d9663e60359920c36018dc8b4cb7d4304148e
lucaslmota <lucaslmota0431@gmail.com>
2023-05-12 14:29:51 -0300

    Pequenos ajustes na Program

--- back/src/DevIo.API/Program.cs
+++ back/src/DevIo.API/Program.cs
@@ -5,24 +5,28 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("ConnStr");
+builder.Services.AddControllers().AddNewtonsoftJson(option => option.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddDbContext<MeuDbContext>(options =>
 {
-    options.UseSqlServer(connectionString);
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConnStr"));
 });
 
 builder.Services.AddIdentityConfiguration(builder.Configuration);
 
-builder.Services.AddControllers().AddNewtonsoftJson(option => option.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 builder.Services.AddAutoMapper(typeof(Program));
+
 builder.Services.ResolveDependencies();
+
 builder.Services.Configure<ApiBehaviorOptions>(options =>
 {
     options.SuppressModelStateInvalidFilter = true;
 });
-builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddCors();
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
